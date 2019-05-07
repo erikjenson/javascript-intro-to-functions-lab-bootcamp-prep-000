d484d6ec37701694854b80e302cbab133684d379
Erik Jenson <erik.a.jenson@gmail.com>
2019-05-07 15:34:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase()=== string) {
   return "YES INDEED!" 
   }
-  if (string === ) {
+  if (string === 'I love you, Grandma.') {
   return "I love you, too."
   }
-  } 
 }
